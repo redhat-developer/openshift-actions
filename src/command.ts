7e41a010c32ee55a0391cdf63a2d7d3a3029740e
Luca Stocchi <lstocchi@redhat.com>
2019-11-19 14:25:10 +0100

    changed way commands get executed

--- src/command.ts
+++ src/command.ts
@@ -4,22 +4,21 @@ import * as split from 'argv-split';
 
 
 export class Command {
-    static async execute(ocPath: string, args: string) {
+    static async execute(ocPath: string, args: string): Promise<number> {
         if (!ocPath) {
             return Promise.reject('Unable to find oc bundle');
         }
 
         const cmdArgs = await Command.prepareOcArgs(args);
-        await exec.exec(ocPath, cmdArgs);
-        return;
+        return await exec.exec(`${ocPath} ${cmdArgs}`);
     }
 
-    static async prepareOcArgs(ocArgs: string) {
+    static async prepareOcArgs(ocArgs: string): Promise<string> {
         let interpolatedArgs = sub(ocArgs, process.env);
         let args = split(interpolatedArgs);
         if (args[0] === 'oc' || args[0] === 'oc.exe') {
             args = args.slice(1);
         }
-        return args;
+        return args.join(' ');
     }
 }
